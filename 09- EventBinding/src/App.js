271a0495bdf8ef49576f1c89cd994f9a9627c0ee
sorted-Tech0 <shivanesforcode@gmail.com>
2021-08-27 02:48:02 +0530

    learning event handler, event binding and props as a method

---
+++ 09- EventBinding/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+
+function App() {
+  return (
+    <div className="App">
+      <EventBind />
+      {/* <FunctionClick />
+      <ClassClick /> */}
+      {/* <Counter /> */}
+      {/* <Message /> */}
+      {/* <Greet name="Bruce" heroname="Batman">
+        <p>This is children props.</p>
+      </Greet>
+      <Greet name="Clark" heroname="Superman">
+        <button>Action</button>
+      </Greet> */}
+      {/* <Greet name="Diana" heroname="Wonderwomen"/>
+      <Welcome name="Bruce" heroname="Batman"/> */}
+      {/* <Welcome name="Clark" heroname="Superman"/>
+      <Welcome name="Diana" heroname="Wonderwomen"/> */}
+      {/* <Hello /> */}
+    </div>
+  );
+}
+
+export default App;
